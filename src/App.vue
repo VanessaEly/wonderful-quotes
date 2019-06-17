0b2639742e037a05be3568e7fc98b87bc76f4024
Vanessa Carla Ely <vely@br.ibm.com>
2019-06-17 16:52:02 -0300

    quotes basic structure added

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <app-quote-grid :quotes="quotes" />
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+
+    export default {
+        components: {
+            appQuoteGrid: QuoteGrid,
+        },
+        data: () => ({
+            maxQuotes: 10,
+            quotes: [
+                'Just a quote to see something',
+            ],
+        }),
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="row">
+        <app-quote v-for="(quote, key) in quotes" :key="key">
+            {{ quote }}
+        </app-quote>
+    </div>
+</template>
+
+<script>
+import Quote from './Quote.vue';
+
+export default {
+    components: {
+        appQuote: Quote,
+    },
+    props: ['quotes'],
+}
+</script>
+
+<style>
+
+</style>
